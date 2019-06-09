862556399cf6079daaeb6a056095ad9c6b6dbe12
Susovan Pati <susovanpati@Susovans-MacBook-Air.local>
2019-06-09 14:02:11 +0530

    finished my Framework

---
+++ MyCoacoaTouchFrameWork/MyCoacoaTouchFrameWork.h
@@ -0,0 +1,19 @@
+//
+//  MyCoacoaTouchFrameWork.h
+//  MyCoacoaTouchFrameWork
+//
+//  Created by Susovan Pati on 09/06/19.
+//  Copyright © 2019 Susovan Pati. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCoacoaTouchFrameWork.
+FOUNDATION_EXPORT double MyCoacoaTouchFrameWorkVersionNumber;
+
+//! Project version string for MyCoacoaTouchFrameWork.
+FOUNDATION_EXPORT const unsigned char MyCoacoaTouchFrameWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCoacoaTouchFrameWork/PublicHeader.h>
+
+
---
+++ MyCoacoaTouchFrameWork/Service.swift
@@ -0,0 +1,16 @@
+//
+//  Service.swift
+//  MyCoacoaTouchFrameWork
+//
+//  Created by Susovan Pati on 09/06/19.
+//  Copyright © 2019 Susovan Pati. All rights reserved.
+//
+
+import Foundation
+public class Service
+{
+    private init() {}
+    
+    public func doSomething () -> String{ return "My cocoaTouch frameWork"}
+
+}
